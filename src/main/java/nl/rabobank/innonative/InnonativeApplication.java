6550591bcaefa7ab46f42d6a462b41702a5b5047
meshkaa <ali.meshkat@rabobank.nl>
2022-11-29 09:39:07 +0100

    init

---
+++ src/main/java/nl/rabobank/innonative/InnonativeApplication.java
@@ -0,0 +1,13 @@
+package nl.rabobank.innonative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnonativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnonativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/rabobank/innonative/InnonativeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rabobank.innonative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnonativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
